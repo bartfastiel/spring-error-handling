24736dcd0e93d05f36c65fad1d2ef47741e43e37
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-07-17 12:27:31 +0200

    Simple example (no details per field)

--- src/main/java/com/example/demo/Handler.java
+++ src/main/java/com/example/demo/Handler.java
@@ -2,13 +2,9 @@ package com.example.demo;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @RestControllerAdvice
@@ -16,17 +12,9 @@ public class Handler {
 
     @ExceptionHandler(BindException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiValidationError handle(BindException e) {
-        List<ApiErrorField> apiErrorFields = new ArrayList<>();
-        for (FieldError fieldError : e.getFieldErrors()) {
-            apiErrorFields.add(new ApiErrorField(
-                    fieldError.getField(),
-                    fieldError.getDefaultMessage()
-            ));
-        }
-        return new ApiValidationError(
-                "Validation failed",
-                apiErrorFields
+    public ApiError handle(BindException e) {
+        return new ApiError(
+                e.getMessage()
         );
     }
 
